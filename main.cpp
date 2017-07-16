3b4f2411bf638927a3345f14d3725f126960420c
fixako <fixako@gmail.com>
2017-07-16 19:09:03 +0300

    f

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "mpg.hpp"
+#include "mpg_view.hpp"
+#include "mpg_model.hpp"
+#include "mpg_controller.hpp"
+
+int main()
+{
+    MpgController mpgc;
+    
+    mpgc.startGame();
+    return 0;
+}
